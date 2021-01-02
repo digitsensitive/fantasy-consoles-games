a661055cf2cb0f85d3c2dce7a8ba15f02eee56d7
digitsensitive <digit.sensitivee@gmail.com>
2021-01-02 01:58:14 +0100

    small update pong

--- src/tic-80/pong/pong.lua
+++ src/tic-80/pong/pong.lua
@@ -3,26 +3,25 @@
 -- desc:   a short written pong clone
 -- script: lua
 
--- global game settings
+-- global game settings --------------------------------------------------------
 local GS = {W = 240, H = 136, S = 8, p1s = 0, p2s = 0, t = 0}
+
+-- game objects ----------------------------------------------------------------
 local p = {
     {x = 2 * GS.S, y = 3 * GS.S, s = 4, c = 12},
     {x = 27.5 * GS.S, y = 3 * GS.S, s = 4, c = 12}
 }
 local b
 
-function init()
-    if math.random() < 0.5 then
-        resetBall(-1)
-    else
-        resetBall(1)
-    end
-end
+-- general definitions and functions -------------------------------------------
+rnd = math.random
 
+-- specific definitions and functions ------------------------------------------
 function resetBall(dir)
     b = {x = GS.W / 2, y = GS.H / 2, vx = 1.8 * dir, vy = 1.8, c = 12}
 end
 
+-- input -----------------------------------------------------------------------
 function input()
     if btn(0) and p[1].y > 0 then
         p[1].y = p[1].y - 1
@@ -31,6 +30,7 @@ function input()
     end
 end
 
+-- update ----------------------------------------------------------------------
 function update()
     b.x = b.x + b.vx
     b.y = b.y + b.vy
@@ -69,6 +69,7 @@ function collisionsCheck()
     end
 end
 
+-- draw ------------------------------------------------------------------------
 function draw()
     cls(14)
     map()
@@ -92,6 +93,16 @@ function draw()
     print("v1.0.0", 200, 130, 5)
 end
 
+-- init ------------------------------------------------------------------------
+function init()
+    if rnd() < 0.5 then
+        resetBall(-1)
+    else
+        resetBall(1)
+    end
+end
+
+-- main ------------------------------------------------------------------------
 init()
 function TIC()
     input()
