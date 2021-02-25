30bab6796198a6f8c36b963e713561abc94a1bdb
Eric <digit.sensitivee@gmail.com>
2021-02-25 23:10:46 +0100

    Update downhill.lua

--- src/tic-80/downhill/downhill.lua
+++ src/tic-80/downhill/downhill.lua
@@ -10,12 +10,12 @@ local GS = {
     HW = 240 / 2,
     HH = 136 / 2,
     t = 0,
-    spawnTime=10,
-    scrollSpeed=1,
-    gameOver=false,
-    shake=30,
-    shakeD=4,
-    cs=nil
+    spawnTime = 10,
+    scrollSpeed = 1,
+    gameOver = false,
+    shake = 30,
+    shakeD = 4,
+    cs = nil
 }
 
 -- game objects ----------------------------------------------------------------
@@ -45,12 +45,12 @@ local SCENES = {
     GAME = 3
 }
 
-local mapArray={}
+local mapArray = {}
 local rndMapGen = {
-    cellsAvg=0,
-    totalAvgs=0,
+    cellsAvg = 0,
+    totalAvgs = 0,
     W = 240,
-    H = GS.H*2
+    H = GS.H * 2
 }
 
 -- general helper functions ----------------------------------------------------
@@ -83,58 +83,60 @@ function collide(a, b)
 end
 
 function shakeScreen()
-    poke(0x3FF9,math.random(-GS.shakeD,GS.shakeD))
-    poke(0x3FF9+1,math.random(-GS.shakeD,GS.shakeD))
-    GS.shake=GS.shake-1		
-    if GS.shake==0 then memset(0x3FF9,0,2) end
+    poke(0x3FF9, math.random(-GS.shakeD, GS.shakeD))
+    poke(0x3FF9 + 1, math.random(-GS.shakeD, GS.shakeD))
+    GS.shake = GS.shake - 1
+    if GS.shake == 0 then
+        memset(0x3FF9, 0, 2)
+    end
 end
 
-function getAverageValueOfSurroundingFields(x,y)
+function getAverageValueOfSurroundingFields(x, y)
     local sum = 0
     local numbFields = 0
     local avg = 0
 
-    if y>1 then
-        sum = sum + mapArray[y-1][x]
+    if y > 1 then
+        sum = sum + mapArray[y - 1][x]
         numbFields = numbFields + 1
 
-        if x>1 then
-            sum = sum + mapArray[y-1][x-1]
+        if x > 1 then
+            sum = sum + mapArray[y - 1][x - 1]
             numbFields = numbFields + 1
         end
 
-        if x<rndMapGen.W then
-            sum = sum + mapArray[y-1][x+1]
-            numbFields = numbFields + 1
-        end
-    end
-
-    if y<rndMapGen.H then
-        sum = sum + mapArray[y+1][x]
-        numbFields = numbFields + 1
-
-        if x>1 then
-            sum = sum + mapArray[y+1][x-1]
-            numbFields = numbFields + 1
-        end
-
-        if x<rndMapGen.W then
-            sum = sum + mapArray[y+1][x+1]
+        if x < rndMapGen.W then
+            sum = sum + mapArray[y - 1][x + 1]
             numbFields = numbFields + 1
         end
     end
 
-    if x>1 then
-        sum = sum + mapArray[y][x-1]
+    if y < rndMapGen.H then
+        sum = sum + mapArray[y + 1][x]
+        numbFields = numbFields + 1
+
+        if x > 1 then
+            sum = sum + mapArray[y + 1][x - 1]
+            numbFields = numbFields + 1
+        end
+
+        if x < rndMapGen.W then
+            sum = sum + mapArray[y + 1][x + 1]
+            numbFields = numbFields + 1
+        end
+    end
+
+    if x > 1 then
+        sum = sum + mapArray[y][x - 1]
         numbFields = numbFields + 1
     end
 
-    if x<rndMapGen.W then
-        sum = sum + mapArray[y][x+1]
+    if x < rndMapGen.W then
+        sum = sum + mapArray[y][x + 1]
         numbFields = numbFields + 1
     end
 
-    avg = sum/numbFields
+    avg = sum / numbFields
 
     rndMapGen.totalAvgs = rndMapGen.totalAvgs + avg
 
@@ -142,37 +144,37 @@ function getAverageValueOfSurroundingFields(x,y)
 end
 
 -- Print text with border
-function printf(t,x,y,c1,c2)
-	local x=x or 0
-	local y=y or 0
-	local c1=c1 or 12
-	local c2=c2 or 0
+function printf(t, x, y, c1, c2)
+    local x = x or 0
+    local y = y or 0
+    local c1 = c1 or 12
+    local c2 = c2 or 0
 
-	print(t,x-1,y,c2)
-	print(t,x,y-1,c2)
-	print(t,x+1,y,c2)
-	print(t,x,y+1,c2)
-	print(t,x,y,c1)
+    print(t, x - 1, y, c2)
+    print(t, x, y - 1, c2)
+    print(t, x + 1, y, c2)
+    print(t, x, y + 1, c2)
+    print(t, x, y, c1)
 end
 
 -- enum for sweetie-16 color palette
 local COLOR = {
-	BLACK = 0,
-	PURPLE = 1,
-	RED = 2,
-	ORANGE = 3,
-	YELLOW = 4,
-	LIGHT_GREEN = 5,
-	GREEN = 6,
-	DARK_GREEN = 7,
-	DARK_BLUE = 8,
-	BLUE = 9,
-	LIGHT_BLUE = 10,
-	CYAN = 11,
-	WHITE = 12,
-	LIGHT_GREY = 13,
-	GREY = 14,
-	DARK_GREY = 15,
+    BLACK = 0,
+    PURPLE = 1,
+    RED = 2,
+    ORANGE = 3,
+    YELLOW = 4,
+    LIGHT_GREEN = 5,
+    GREEN = 6,
+    DARK_GREEN = 7,
+    DARK_BLUE = 8,
+    BLUE = 9,
+    LIGHT_BLUE = 10,
+    CYAN = 11,
+    WHITE = 12,
+    LIGHT_GREY = 13,
+    GREY = 14,
+    DARK_GREY = 15
 }
 
 -- init ------------------------------------------------------------------------
@@ -185,29 +187,29 @@ end
 -- Generating 2D height maps the easy way
 -- https://dxprog.com/files/randmaps.html
 function generateRandomMap()
-    for y=1,rndMapGen.H do
-        mapArray[y]={}
-        for x=1,rndMapGen.W do
-            mapArray[y][x]=rnd(1,255)
+    for y = 1, rndMapGen.H do
+        mapArray[y] = {}
+        for x = 1, rndMapGen.W do
+            mapArray[y][x] = rnd(1, 255)
         end
     end
 
-    for y=1,rndMapGen.H do
-        for x=1,rndMapGen.W do
-            mapArray[y][x]=getAverageValueOfSurroundingFields(x,y)
+    for y = 1, rndMapGen.H do
+        for x = 1, rndMapGen.W do
+            mapArray[y][x] = getAverageValueOfSurroundingFields(x, y)
         end
     end
 
     rndMapGen.cellsAvg = rndMapGen.totalAvgs / (rndMapGen.H * rndMapGen.W)
 
-    for y=1,rndMapGen.H do
-        for x=1,rndMapGen.W do
-            if mapArray[y][x] < rndMapGen.cellsAvg+50 then
-                mapArray[y][x]=12
-            elseif mapArray[y][x] >= rndMapGen.cellsAvg+50 and mapArray[y][x] < rndMapGen.cellsAvg+60 then
-                mapArray[y][x]=13
+    for y = 1, rndMapGen.H do
+        for x = 1, rndMapGen.W do
+            if mapArray[y][x] < rndMapGen.cellsAvg + 50 then
+                mapArray[y][x] = 12
+            elseif mapArray[y][x] >= rndMapGen.cellsAvg + 50 and mapArray[y][x] < rndMapGen.cellsAvg + 60 then
+                mapArray[y][x] = 13
             else
-                mapArray[y][x]=14
+                mapArray[y][x] = 14
             end
         end
     end
@@ -275,9 +277,17 @@ function spawnNewTrails()
 end
 
 function spawnNewTrees()
-    if GS.t%GS.spawnTime == 0 then
-        ins(obj, {
-            type = OBJ_TYPE.TREE, id = rnd(0,3), x = rnd(0,GS.W-8), y = GS.H, mov = true})
+    if GS.t % GS.spawnTime == 0 then
+        ins(
+            obj,
+            {
+                type = OBJ_TYPE.TREE,
+                id = rnd(0, 3),
+                x = rnd(0, GS.W - 8),
+                y = GS.H,
+                mov = true
+            }
+        )
     end
 end
 
@@ -295,26 +305,27 @@ function updateObjects()
         end
 
         if o.type == OBJ_TYPE.TREE then
-            if collide(o,p) then
+            if collide(o, p) then
                 GS.gameOver = true
                 p.scale = 1.2
-                -- shakeScreen()
+            -- shakeScreen()
             end
         end
     end
 end
 
 function updatePlayer()
+    -- update horizontal velocity with applied friction
     if abs(p.vx) > 0 then
         p.x = p.x + p.vx
         p.vx = p.vx * 0.9
     end
 
-    -- swap 
+    -- check left and right screen border collision
     if p.x < 0 then
         p.x = 0
-    elseif p.x > GS.W-p.W then
-        p.x = GS.W-p.W
+    elseif p.x > GS.W - p.W then
+        p.x = GS.W - p.W
     end
 end
 
@@ -327,17 +338,17 @@ function gameSceneDraw()
 end
 
 function drawMap()
-    for y=1,GS.H do
-        for x=1,GS.W do
-            pix(x,(y-GS.t)%GS.H, mapArray[y][x])
+    for y = 1, GS.H do
+        for x = 1, GS.W do
+            pix(x, (y - GS.t) % GS.H, mapArray[y][x])
         end
     end
 end
 
 function drawPlayer()
     local animSpeed = 128
-    local id = (time()//animSpeed)%2
-    spr(p.id+id, p.x, p.y, 0, p.scale, p.flip)
+    local id = (time() // animSpeed) % 2
+    spr(p.id + id, p.x, p.y, 0, p.scale, p.flip)
 end
 
 function drawObjects()
@@ -352,5 +363,5 @@ function drawObjects()
 end
 
 function drawUI()
-    printf("Score: " .. GS.t,5,5,COLOR.YELLOW,COLOR.DARK_GREY)
+    printf("Score: " .. GS.t, 5, 5, COLOR.YELLOW, COLOR.DARK_GREY)
 end
